bbd9029ac3d871f06d4da3674d3367799aadb606
Guido Cioni <guidocioni@gmail.com>
2020-10-01 19:53:05 +0200

    Added parallelized extraction of files and background downloading Now even downloading 3D variables is much faster!

--- functions_download_dwd.sh
+++ functions_download_dwd.sh
@@ -1,10 +1,20 @@
 #Given a variable name and year-month-day-run as environmental variables download and merges the variable
+parallelized_extraction(){
+	# You need to pass a glob patter which will be tested for files
+	until [ `ls -1 ${1}.bz2 2>/dev/null | wc -l ` -gt 0 ]; do
+	     sleep 1
+	done
+	# 
+	while [ `ls -1 ${1}.bz2 2>/dev/null | wc -l ` -gt 0 ]; do
+		ls ${1}.bz2| parallel -j+0 bzip2 -d '{}' 
+	    sleep 1
+	done
+}
 download_merge_2d_variable_icon_globe()
 {
 	filename="icon_global_icosahedral_single-level_${year}${month}${day}${run}_*_${1}.grib2"
-	wget -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon/grib/${run}/${1,,}/"
-	echo 'Extracting files'
-	bzip2 -d ${filename}.bz2 
+	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon/grib/${run}/${1,,}/"
+	parallelized_extraction ${filename}
 	cdo -f nc copy -mergetime ${filename} ${1}_${year}${month}${day}${run}_global.nc
 	rm ${filename}
 }
@@ -14,15 +24,12 @@ download_merge_3d_variable_icon_globe()
 {
 	# Parallelize this part by getting a file list and dividing into chunks ##############
 	filename="icon_global_icosahedral_pressure-level_${year}${month}${day}${run}_*_${1}.grib2"
-	wget -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon/grib/${run}/${1,,}/"
-	echo 'Extracting files'
-	bzip2 -d ${filename}.bz2
+	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon/grib/${run}/${1,,}/"
+	parallelized_extraction ${filename}
 	#######################
-	# Parallelize this loop
 	for level in "300" "500" "700" "850" "950" "1000" ; do
 		(
 		 cdo -f nc copy -mergetime icon_global_icosahedral_pressure-level_${year}${month}${day}${run}_*_${level}_${1}.grib2 icon_global_icosahedral_pressure-level_${year}${month}${day}${run}_${level}_${1}.nc
-		 rm icon_global_icosahedral_pressure-level_${year}${month}${day}${run}_*_${level}_${1}.grib2
 		) &
 	done
 	wait
@@ -31,6 +38,7 @@ download_merge_3d_variable_icon_globe()
 	files_to_merge=`ls -v icon_global_icosahedral_pressure-level_${year}${month}${day}${run}_*_${1}.nc` 
 	cdo merge ${files_to_merge} ${1}_${year}${month}${day}${run}_global.nc
 	rm icon_global_icosahedral_pressure-level_${year}${month}${day}${run}_*_${1}.nc
+	rm ${filename}
 }
 export -f download_merge_3d_variable_icon_globe
 ################################################
@@ -53,9 +61,8 @@ export -f download_invariant_icon_globe
 download_merge_soil_variable_icon_globe()
 {
 	filename="icon_global_icosahedral_soil-level_${year}${month}${day}${run}_*_3_${1}.grib2"
-	wget -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon-eu/grib/${run}/${1,,}/"
-	echo 'Extracting files'
-	bzip2 -d ${filename}.bz2
+	wget -b -r -nH -np -nv -nd --reject "index.html*" --cut-dirs=3 -A "${filename}.bz2" "https://opendata.dwd.de/weather/nwp/icon-eu/grib/${run}/${1,,}/"
+	parallelized_extraction ${filename}
 	cdo -f nc copy -mergetime ${filename} ${1}_${year}${month}${day}${run}_global.nc
 	rm ${filename}
 }
