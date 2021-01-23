Added PV computation and plots + fixed a few things

plotting/plot_geop_500_remap.py
@@ -34,7 +34,7 @@ def main():
                         projection=projection, remapped=True)
 
     levels_temp = np.arange(-34., 36., 2.)
-    levels_gph = np.arange(4700., 6000., 70.)
+    levels_gph = np.arange(4700., 6000., 80.)
 
     cmap = get_colormap('temp_meteociel')
 
@@ -102,9 +102,9 @@ def plot_files(dss, **args):
 
 
         maxlabels = plot_maxmin_points(args['ax'], args['x'], args['y'], data['geop'],
-                                        'max', 160, symbol='H', color='royalblue', random=True)
+                                        'max', 250, symbol='H', color='royalblue', random=True)
         minlabels = plot_maxmin_points(args['ax'], args['x'], args['y'], data['geop'],
-                                        'min', 160, symbol='L', color='coral', random=True)
+                                        'min', 250, symbol='L', color='coral', random=True)
 
 
         an_fc = annotation_forecast(args['ax'], time)

plotting/plot_geop_50_remap.py
@@ -14,7 +14,7 @@ if not debug:
 import matplotlib.pyplot as plt
 
 # The one employed for the figure name when exported 
-variable_name = 'gph_50'
+variable_name = 'gpt_50'
 
 print_message('Starting script to plot '+variable_name)
 
@@ -34,7 +34,7 @@ def main():
                         projection=projection, remapped=True)
 
     levels_temp = np.arange(-86, -18, 1)
-    levels_gph = np.arange(1970., 2050., 25.)
+    levels_gph = np.arange(19700., 20500., 250.)
 
     cmap = get_colormap('temp_meteociel')
 
@@ -54,7 +54,7 @@ def main():
 
     print_message('Pre-processing finished, launching plotting scripts')
     if debug:
-        plot_files(dset.isel(time=slice(0, 2)), **args)
+        plot_files(dset.isel(time=slice(-2, -1)), **args)
     else:
         # Parallelize the plotting by dividing into chunks and processes
         dss = chunks_dataset(dset, chunks_size)

plotting/plot_pv_250_remap.py
@@ -3,7 +3,7 @@ from multiprocessing import Pool
 from functools import partial
 from utils import *
 import sys
-from computations import compute_geopot_height, compute_wind_speed
+from computations import compute_spacing, compute_theta, compute_pv
 
 debug = False
 if not debug:
@@ -13,7 +13,7 @@ if not debug:
 import matplotlib.pyplot as plt
 
 # The one employed for the figure name when exported 
-variable_name = 'winds_jet'
+variable_name = 'pv_250'
 
 print_message('Starting script to plot '+variable_name)
 
@@ -30,13 +30,16 @@ else:
 def main():
     """In the main function we basically read the files and prepare the variables to be plotted.
     This is not included in utils.py as it can change from case to case."""
-    dset = read_dataset(variables=['U', 'V', 'FI'], level=30000,
-        projection=projection, remapped=True)
+    dset = read_dataset(variables=['U', 'V', 'T', 'PMSL'],
+                        level=[15000, 25000, 30000],
+                        projection=projection, remapped=True)
 
-    # Select 850 hPa level using metpy
-    levels_wind = np.arange(80., 300., 10.)
-    levels_gph = np.arange(8200., 9700., 100.)
-    cmap = truncate_colormap(plt.get_cmap('CMRmap_r'), 0., 0.9)
+    if projection != 'world':
+        levels_pv = np.linspace(-0.5e-5, 1.6e-5, 30)
+    else:
+        levels_pv = np.linspace(-1.5e-5, 1.6e-5, 50)
+
+    cmap = get_colormap('temp')
 
     _ = plt.figure(figsize=(figsize_x, figsize_y))
     ax = plt.gca()
@@ -44,15 +47,20 @@ def main():
     m.fillcontinents(color='lightgray', lake_color='whitesmoke', zorder=0)
     # Subset dataset only on the area
     dset = dset.where(mask, drop=True)
-    dset = compute_wind_speed(dset)
-    dset = compute_geopot_height(dset)
+    dset = compute_spacing(dset)
+    dset = compute_theta(dset)
 
-    dset = dset.drop(['z', 'u', 'v']).load()
+    dset = dset.load()
+
+    dset['prmsl'].metpy.convert_units('hPa')
+    levels_mslp = np.arange(dset['prmsl'].min().astype("int"),
+        dset['prmsl'].max().astype("int"), 5.)
 
     # All the arguments that need to be passed to the plotting function
     args = dict(x=x, y=y, ax=ax,
-             levels_wind=levels_wind, levels_gph=levels_gph,
-             time=dset.time, cmap=cmap)
+             levels_pv=levels_pv,
+             levels_mslp=levels_mslp,
+             cmap=cmap)
 
     print_message(sys.argv[0]+': Pre-processing finished, launching plotting scripts')
     if debug:
@@ -70,40 +78,41 @@ def plot_files(dss, **args):
     first = True
     for time_sel in dss.time:
         data = dss.sel(time=time_sel)
+        data = compute_pv(data)
         time, run, cum_hour = get_time_run_cum(data)
         # Build the name of the output image
         filename = subfolder_images[projection] + '/' + variable_name + '_%s.png' % cum_hour
 
         cs = args['ax'].contourf(args['x'], args['y'],
-                                    data['wind_speed'],
+                                    data['pv'].sel(plev=25000),
                                     extend='max', cmap=args['cmap'],
-                                    levels=args['levels_wind'])
+                                    levels=args['levels_pv'])
 
-        # Unfortunately m.contour with tri = True doesn't work because of a bug 
-        c = args['ax'].contour(args['x'], args['y'], data['geop'],
-                             levels=args['levels_gph'], colors='black', linewidths=0.5)
+        c = args['ax'].contour(args['x'], args['y'], data['prmsl'],
+                             levels=args['levels_mslp'], colors='white', linewidths=1)
 
-
-        maxlabels = plot_maxmin_points(args['ax'], args['x'], args['y'], data['geop'],
-                                        'max', 250, symbol='H', color='royalblue', random=True)
-        minlabels = plot_maxmin_points(args['ax'], args['x'], args['y'], data['geop'],
-                                        'min', 250, symbol='L', color='coral', random=True)
 
         labels = args['ax'].clabel(c, c.levels, inline=True, fmt='%4.0f' , fontsize=5)
+
+        maxlabels = plot_maxmin_points(args['ax'], args['x'], args['y'], data['prmsl'],
+                                        'max', 180, symbol='H', color='royalblue', random=True)
+        minlabels = plot_maxmin_points(args['ax'], args['x'], args['y'], data['prmsl'],
+                                        'min', 180, symbol='L', color='coral', random=True)
+
         an_fc = annotation_forecast(args['ax'], time)
-        an_var = annotation(args['ax'], 'Winds [kph] and geopotential [m] @300hPa',
+        an_var = annotation(args['ax'], 'PV @ 250 hPa and MSLP (hPa)',
             loc='lower left', fontsize=6)
         an_run = annotation_run(args['ax'], run)
 
         if first:
-            plt.colorbar(cs, orientation='horizontal', label='Wind', pad=0.03, fraction=0.02)
+            plt.colorbar(cs, orientation='horizontal', label='PV [%s]' % data['pv'].units, pad=0.03, fraction=0.035)
 
         if debug:
             plt.show(block=True)
         else:
             plt.savefig(filename, **options_savefig)        
 
-        remove_collections([c, cs, labels, an_fc, an_var, an_run, maxlabels, minlabels])
+        remove_collections([cs, c, labels, maxlabels, minlabels, an_fc, an_var, an_run])
 
         first = False
 